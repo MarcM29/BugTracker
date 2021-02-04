204c84253e80b4d3515dd43565866311f140c024
Marc <47401977+MarcM29@users.noreply.github.com>
2021-02-03 21:50:42 -0500

    Devs can create, edit and delete tickets
    Devs must log in before they have access to add, create or delete tickets.
    Tickets store id, title, desc, priority, creators username, date. 
    NEXT STEP IS TO ADD A PAGE THAT LISTS DEVS/MEMBERS and then using this can begin adding functionality of assigning devs to tickets + when tickets are "Deleted" the site keeps track of # of closed tickets

---
+++ BugTracker/Data/Migrations/20210204003809_initialDBsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Data.Migrations
+{
+    public partial class initialDBsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TicketTitle = table.Column<string>(nullable: false),
+                    TicketDescription = table.Column<string>(nullable: false),
+                    TicketPriority = table.Column<string>(nullable: false),
+                    UsersName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket");
+        }
+    }
+}
